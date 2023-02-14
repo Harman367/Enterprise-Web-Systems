6c9f8b7feb2155bb9044ab4c61fe8887709d70ff
Harman Singh <harman98367@gmail.com>
2023-02-14 18:52:36 +0000

    Modified openDropDown() to close other dropdown. Function to close dropdown when clicked outside of it.

--- src/public/script.js
+++ src/public/script.js
@@ -26,17 +26,17 @@ function removeInput(element) {
 //Add event listener to the button.
 window.addEventListener('load', () => {
     //Add event listener to the add worker button.
-    document.getElementById('addWorker').addEventListener('click', e => {
+    document.getElementById('addWorker').addEventListener('click', event => {
         createInput('worker', 'workers', 'workerTemplate');
     });
 
     //Add event listener to the add ongoing cost button.
-    document.getElementById('addOngoing').addEventListener('click', e => {
+    document.getElementById('addOngoing').addEventListener('click', event => {
         createInput('ongoingCost', 'ongoingCosts', 'ongoingTemplate');
     });
 
     //Add event listener to the add one off cost button.
-    document.getElementById('addOneOff').addEventListener('click', e => {
+    document.getElementById('addOneOff').addEventListener('click', event => {
         createInput('oneOffCost', 'oneOffCosts', 'oneOffTemplate');
     });
 });
@@ -45,6 +45,28 @@ window.addEventListener('load', () => {
 //Navbar script-------------------------------------------------------------------------------------
 
 //Function to open thre dropdown menu on click.
-function openDropdown(id) {
+function openDropdown(id, close) {
+    //Open the dropdown menu.
     document.getElementById(id).classList.toggle("show");
+
+    //Close the other dropdown menu.
+    document.getElementById(close).classList.remove("show");
+}
+
+//Function to close the dropdown menu when the user clicks outside of it.
+window.onclick = function (event) {
+    //Check if the user clicked outside of the dropdown menu.
+    if (!event.target.matches('.dropdown *')) {
+
+        //Get all dropdown menus.
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+
+        //Close all dropdown menus.
+        for (let i = 0; i < dropdowns.length; i++) {
+            let open = dropdowns[i];
+            if (open.classList.contains('show')) {
+                open.classList.remove('show');
+            }
+        }
+    }
 }
