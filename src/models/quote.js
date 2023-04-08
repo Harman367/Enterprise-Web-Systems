4e10f81a6200413868dda4494281cf75f1d829b5
Harman Singh <harman98367@gmail.com>
2023-04-08 03:48:12 +0100

    Finished quote schema.

--- src/models/quote.js
+++ src/models/quote.js
@@ -1,2 +1,12 @@
 //Imports
 import { model, Schema } from "mongoose";
+
+//Create Schema
+const quoteSchema = new Schema({
+    "name": { type: String, required: true },
+    "subtasks": [{ type: Schema.Types.ObjectId, ref: "Subtask" }]
+});
+
+//Create model
+const quoteModel = model("Quote", quoteSchema);
+export default quoteModel;
