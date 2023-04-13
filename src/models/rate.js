145e0251aa697cf663cc7af1b3bac2b9dd05a9fc
Harman Singh <harman98367@gmail.com>
2023-04-13 02:28:58 +0100

    Finished the model

--- src/models/rate.js
+++ src/models/rate.js
@@ -1,2 +1,13 @@
 //Imports
 import { model, Schema } from "mongoose";
+
+//Create Schema
+const rateSchema = new Schema({
+    "junior": { type: Number, required: true },
+    "standard": { type: Number, required: true },
+    "senior": { type: Number, required: true }
+});
+
+//Create model
+const rateModel = model("Rate", rateSchema);
+export default rateModel;
